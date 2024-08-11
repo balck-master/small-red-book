34a7048765974d8555c9a37a96fbc450b73febab
唐炒栗子怡 <2905144632@qq.com>
2024-08-11 16:16:49 +0800

    add 对象存储框架

---
+++ smallred-oss/smallredbook-oss-biz/src/main/java/org/example/smallredbook/oss/biz/XiaohashuOssBizApplication.java
@@ -0,0 +1,13 @@
+package org.example.smallredbook.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohashuOssBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohashuOssBizApplication.class, args);
+    }
+
+}
