bd794ff8e9aebeb04ee57f2dfbecfe79e7ed20b5
唐炒栗子怡 <2905144632@qq.com>
2024-09-05 11:48:44 +0800

    add 分布式id微服务搭建

--- smallredbook-user/smallredbook-user-biz/src/test/java/org/example/smallredbook/user/biz/DruidTest.java
+++ smallredbook-user/smallredbook-user-biz/src/test/java/org/example/smallredbook/user/biz/DruidTest.java
@@ -46,9 +46,13 @@ public class DruidTest {
         log.info("解密后的密码：{}",decrypt);
 
 //        privateKey: MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEAj8BzfSdG/eBapQLz6u6nTLP93zbK0i8Vcv3+oW2ednPoCpxiaCnw6zIeETJ8v9U3AdfWQuQB+dBBy8FC/lFFLQIDAQABAkAL0OrtT6wzNBzfUXXRStqvoF/u2Cclzb76m8wxYrxSyiXr6Vjb2IkYRGZURtxgwkPYAi45mcb2BBXpBzq0gyz1AiEAmfGevGrt4wJ2jjae9AlcHg85osf4K55BWQBFuqec4BMCIQDvDRj7/pgPM5qBhIFZfoJiEh+v2upKOcSczVRKz2ptvwIhAIFoFgrHTE+++1AS5E26LIBsRgPrW1e3QbGCRBti2MGXAiEA5qeJmuP9znUpOJ0kvbhej8gTAPhGphX1SqKoGYLmmzsCIQCN7WhLFdSfgN6go8GoMDTTcL9vZl+/2TqmeSBDqdCaCA==
+//        密码为：123456时
 //        publicKey: MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAI/Ac30nRv3gWqUC8+rup0yz/d82ytIvFXL9/qFtnnZz6AqcYmgp8OsyHhEyfL/VNwHX1kLkAfnQQcvBQv5RRS0CAwEAAQ==
-//        加密后的密码: aNpKQgGzm7rlgpC4ZTx3sdvJnPfT+SXSokxF3KWKjrDjNT+MkJHv/nOf3F/MGPgXAOGluRQZ+7KcJrVur5DOqg==
-//        解密后的密码：123456
+//        加密后的密码：A2qT03X7KlL4v/F2foD6kV/Ch9gpNBWOh1qoCywanjv1AsI7f9x3iAyR9NkUKeV+FMo+halCTzy5Llbk2VOrVQ==
+
+//        密码为：956764c5e473f4d3
+//        publicKey: MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAI8JGMGfXsNXGLtEG9BPrloN4YKCI88Ihn8gJzaokCqB5m0iV3Q2UoEv/M9iFyd9S9BO6rVqP5QpaFybyaW63MkCAwEAAQ==
+//        加密后的密码：ImBl6+6zoFuywJKzwo1UWkyRa8AhGj/pfzkDZPzn3cGWVyv3SbV8YoWoGLJ0jJslc8yWFmVv80JQWMwXg5V6Fg==
     }
 
     @Test
