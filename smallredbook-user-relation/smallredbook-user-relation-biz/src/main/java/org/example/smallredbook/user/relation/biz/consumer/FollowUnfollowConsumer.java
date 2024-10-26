7a96e83bfddb7dc86533c4e614368a403fad19c2
唐炒栗子怡 <2905144632@qq.com>
2024-10-26 14:18:57 +0800

    add 发送携带tag标签的MQ消息+消费者开发

---
+++ smallredbook-user-relation/smallredbook-user-relation-biz/src/main/java/org/example/smallredbook/user/relation/biz/consumer/FollowUnfollowConsumer.java
@@ -0,0 +1,100 @@
+package org.example.smallredbook.user.relation.biz.consumer;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.example.framework.common.utils.JsonUtils;
+import org.example.smallredbook.user.relation.biz.constant.MQConstants;
+
+import org.example.smallredbook.user.relation.biz.domain.dataobject.FansDO;
+import org.example.smallredbook.user.relation.biz.domain.dataobject.FollowingDO;
+import org.example.smallredbook.user.relation.biz.domain.mapper.FansDOMapper;
+import org.example.smallredbook.user.relation.biz.domain.mapper.FollowingDOMapper;
+import org.example.smallredbook.user.relation.biz.model.dto.FollowUserMqDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * @Author: tzy
+ * @Description: 关注 、取关的消费者
+ * @Date: 2024/10/25 19:33
+ */
+@Component
+@Slf4j
+//不知道发送模式,默认就是 点对点模式=>只有一个服务实例可以消费
+@RocketMQMessageListener(consumerGroup = "xiaohashu_group" ,
+                        topic = MQConstants.TOPIC_FOLLOW_OR_UNFOLLOW)
+public class FollowUnfollowConsumer implements RocketMQListener<Message> {
+
+    @Resource
+    private TransactionTemplate transactionTemplate;
+    @Resource
+    private FollowingDOMapper followingDOMapper;
+    @Resource
+    private FansDOMapper fansDOMapper;
+    @Override
+    public void onMessage(Message message) {
+        //消息体
+        String bodyJsonStr = new String(message.getBody());
+        String tags = message.getTags();
+
+        log.info("==> FollowUnfollowConsumer 消费者 消费了消息{}，tags:{}",bodyJsonStr,tags);
+
+        //根据MQ 标签，判断操作类型
+        if(Objects.equals(tags,MQConstants.TAG_FOLLOW)){//关注
+            handleFollowTagMessage(bodyJsonStr);
+        }else if(Objects.equals(tags,MQConstants.TAG_UNFOLLOW)){//取关
+            //todo
+        }
+    }
+
+    /**
+     * 处理关注 的tag消息
+     * @param bodyJsonStr
+     */
+    private void handleFollowTagMessage(String bodyJsonStr) {
+        //将消息体Json转成DTO对象
+        FollowUserMqDTO followUserMqDTO = JsonUtils.parseObject(bodyJsonStr, FollowUserMqDTO.class);
+        if(Objects.isNull(followUserMqDTO)) return;
+
+        //幂等性：通过联合唯一索引判断
+        Long userId = followUserMqDTO.getUserId();
+        Long followUserId = followUserMqDTO.getFollowUserId();
+        LocalDateTime createTime = followUserMqDTO.getCreateTime();
+
+        //编程式事务 : 关注表插入一条记录，同时往粉丝表插入一条记录。为了保证原子性
+        boolean isSuccess = Boolean.TRUE.equals(transactionTemplate.execute(status -> {
+            try {
+
+                int count = followingDOMapper.insert(FollowingDO.builder()
+                        .followingUserId(followUserId)
+                        .userId(userId)
+                        .createTime(createTime)
+                        .build());
+
+                if(count >1 ) {
+                    fansDOMapper.insert(FansDO.builder()
+                            .userId(followUserId)
+                            .fansUserId(userId)
+                            .createTime(createTime)
+                            .build());
+                    }
+                return true;
+            }catch (Exception e){
+                status.setRollbackOnly();// 标记事务为回滚
+                log.error("",e);
+            }
+            return false;
+        }));
+
+        log.error("## 数据库添加记录结果：{}",isSuccess);
+        //todo: 跟新redis中被关注用户的 ZSet粉丝列表
+
+    }
+}
--- smallredbook-user-relation/smallredbook-user-relation-biz/src/main/java/org/example/smallredbook/user/relation/biz/service/impl/RelationServiceImpl.java
+++ smallredbook-user-relation/smallredbook-user-relation-biz/src/main/java/org/example/smallredbook/user/relation/biz/service/impl/RelationServiceImpl.java
@@ -3,23 +3,33 @@ package org.example.smallredbook.user.relation.biz.service.impl;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.RandomUtil;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.example.framework.biz.context.holder.LoginUserContextHolder;
 import org.example.framework.common.exception.BizException;
 import org.example.framework.common.response.Response;
 import org.example.framework.common.utils.DateUtils;
+import org.example.framework.common.utils.JsonUtils;
 import org.example.smallredbook.user.api.dto.resp.FindUserByIdRspDTO;
+import org.example.smallredbook.user.relation.biz.constant.MQConstants;
 import org.example.smallredbook.user.relation.biz.constant.RedisKeyConstants;
 import org.example.smallredbook.user.relation.biz.domain.dataobject.FollowingDO;
 import org.example.smallredbook.user.relation.biz.domain.mapper.FollowingDOMapper;
 import org.example.smallredbook.user.relation.biz.enums.LuaResultEnum;
 import org.example.smallredbook.user.relation.biz.enums.ResponseCodeEnum;
+import org.example.smallredbook.user.relation.biz.model.dto.FollowUserMqDTO;
 import org.example.smallredbook.user.relation.biz.model.vo.FollowUserReqVO;
 import org.example.smallredbook.user.relation.biz.rpc.UserRpcService;
 import org.example.smallredbook.user.relation.biz.service.RelationService;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -31,6 +41,8 @@ import java.util.Objects;
  * @Description:
  * @Date: 2024/10/25 15:08
  */
+@Service
+@Slf4j
 public class RelationServiceImpl implements RelationService {
     @Resource
     private UserRpcService userRpcService;
@@ -39,92 +51,113 @@ public class RelationServiceImpl implements RelationService {
     @Resource
     private FollowingDOMapper followingDOMapper;
 
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
     /**
      * 关注用户
      * @param followUserReqVO
      * @return
      */
     public Response<?> follow(FollowUserReqVO followUserReqVO) {
-        //关注用户id
+        // 关注的用户 ID
         Long followUserId = followUserReqVO.getFollowUserId();
-        //当前登录用户id
+
+        // 当前登录的用户 ID
         Long userId = LoginUserContextHolder.getUserId();
 
-        //校验：不能关注自己
-        if(Objects.equals(userId,followUserId)){
+        // 校验：无法关注自己
+        if (Objects.equals(userId, followUserId)) {
             throw new BizException(ResponseCodeEnum.CANT_FOLLOW_YOUR_SELF);
         }
 
-        //  校验关注的用户是否存在
-        FindUserByIdRspDTO findUserByIdRspDTO = userRpcService.findById(userId);
-        if(!Objects.isNull(findUserByIdRspDTO)){
+        // 1.校验关注的用户是否存在
+        FindUserByIdRspDTO findUserByIdRspDTO = userRpcService.findById(followUserId);
+
+        if (Objects.isNull(findUserByIdRspDTO)) {
             throw new BizException(ResponseCodeEnum.FOLLOW_USER_NOT_EXISTED);
         }
 
-        // TODO: 校验关注数是否已经达到上限
         // 构建当前用户关注列表的 Redis Key
         String followingRedisKey = RedisKeyConstants.buildUserFollowingKey(userId);
 
         DefaultRedisScript<Long> script = new DefaultRedisScript<>();
-        // Lua 脚本路径 + 返回值类型
+        // Lua 脚本路径
         script.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/follow_check_and_add.lua")));
+        // 返回值类型
         script.setResultType(Long.class);
 
-        // 当前时间 转化为时间戳
+        // 当前时间
         LocalDateTime now = LocalDateTime.now();
+        // 当前时间转时间戳
         long timestamp = DateUtils.localDateTime2Timestamp(now);
 
         // 执行 Lua 脚本，拿到返回结果
         Long result = (Long) redisTemplate.execute(script, Collections.singletonList(followingRedisKey), followUserId, timestamp);
 
-        LuaResultEnum luaResultEnum = LuaResultEnum.valueOf(result);
-        if(Objects.isNull(luaResultEnum)){
-            throw new RuntimeException("Lua 返回结果错误");
-        }
+        // 校验 Lua 脚本执行结果
+        checkLuaScriptResult(result);
 
-        switch (luaResultEnum){
-            // 关注数已达到上限
-            case FOLLOW_LIMIT -> throw new BizException(ResponseCodeEnum.FOLLOWING_COUNT_LIMIT);
-            // 已经关注了该用户
-            case ALREADY_FOLLOWED -> throw new BizException(ResponseCodeEnum.ALREADY_FOLLOWED);
-            //ZSet 关注列表不存在
-            case ZSET_NOT_EXIST -> {
-                // 从数据库查询当前用户的关注关系记录
-                List<FollowingDO> followingDOS = followingDOMapper.selectByUserId(userId);
+        // ZSET 不存在
+        if (Objects.equals(result, LuaResultEnum.ZSET_NOT_EXIST.getCode())) {
+            // 从数据库查询当前用户的关注关系记录
+            List<FollowingDO> followingDOS = followingDOMapper.selectByUserId(userId);
 
-                // 随机过期时间
-                // 保底1天+随机秒数
-                long expireSeconds = 60*60*24 + RandomUtil.randomInt(60*60*24);
+            // 随机过期时间
+            // 保底1天+随机秒数
+            long expireSeconds = 60*60*24 + RandomUtil.randomInt(60*60*24);
 
-                // 若记录为空，直接 ZADD 对象, 并设置过期时间
-                if (CollUtil.isEmpty(followingDOS)) {
-                    DefaultRedisScript<Long> script2 = new DefaultRedisScript<>();
-                    script2.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/follow_add_and_expire.lua")));
-                    script2.setResultType(Long.class);
+            // 若记录为空，直接 ZADD 对象, 并设置过期时间
+            if (CollUtil.isEmpty(followingDOS)) {
+                DefaultRedisScript<Long> script2 = new DefaultRedisScript<>();
+                script2.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/follow_add_and_expire.lua")));
+                script2.setResultType(Long.class);
 
-                    // TODO: 可以根据用户类型，设置不同的过期时间，若当前用户为大V, 则可以过期时间设置的长些或者不设置过期时间；如不是，则设置的短些
-                    // 如何判断呢？可以从计数服务获取用户的粉丝数，目前计数服务还没创建，则暂时采用统一的过期策略
-                    redisTemplate.execute(script2, Collections.singletonList(followingRedisKey), followUserId, timestamp, expireSeconds);
-                } else { // 若记录不为空，则将关注关系数据全量同步到 Redis 中，并设置过期时间；
-                    // 构建 Lua 参数
-                    Object[] luaArgs = buildLuaArgs(followingDOS, expireSeconds);
+                // TODO: 可以根据用户类型，设置不同的过期时间，若当前用户为大V, 则可以过期时间设置的长些或者不设置过期时间；如不是，则设置的短些
+                // 如何判断呢？可以从计数服务获取用户的粉丝数，目前计数服务还没创建，则暂时采用统一的过期策略
+                redisTemplate.execute(script2, Collections.singletonList(followingRedisKey), followUserId, timestamp, expireSeconds);
+            } else { // 若记录不为空，则将关注关系数据全量同步到 Redis 中，并设置过期时间；
+                // 构建 Lua 参数
+                Object[] luaArgs = buildLuaArgs(followingDOS, expireSeconds);
 
-                    // 执行 Lua 脚本，批量同步关注关系数据到 Redis 中
-                    DefaultRedisScript<Long> script3 = new DefaultRedisScript<>();
-                    script3.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/follow_batch_add_and_expire.lua")));
-                    script3.setResultType(Long.class);
-                    redisTemplate.execute(script3, Collections.singletonList(followingRedisKey), luaArgs);
+                // 执行 Lua 脚本，批量同步关注关系数据到 Redis 中
+                DefaultRedisScript<Long> script3 = new DefaultRedisScript<>();
+                script3.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/follow_batch_add_and_expire.lua")));
+                script3.setResultType(Long.class);
+                redisTemplate.execute(script3, Collections.singletonList(followingRedisKey), luaArgs);
 
-                    // 再次调用上面的 Lua 脚本：follow_check_and_add.lua , 将最新的关注关系添加进去
-                    result = (Long) redisTemplate.execute(script, Collections.singletonList(followingRedisKey), followUserId, timestamp);
-                    checkLuaScriptResult(result);
-                }
+                // 再次调用上面的 Lua 脚本：follow_check_and_add.lua , 将最新的关注关系添加进去
+                result = (Long) redisTemplate.execute(script, Collections.singletonList(followingRedisKey), followUserId, timestamp);
+                checkLuaScriptResult(result);
             }
         }
 
-        // TODO: 写入 Redis ZSET 关注列表
-
         // TODO: 发送 MQ
+        // 构建消息对象
+        FollowUserMqDTO followUserMqDTO = FollowUserMqDTO.builder()
+                .userId(userId)
+                .followUserId(followUserId)
+                .createTime(LocalDateTime.now())
+                .build();
+        //将 DTO 转成JSON 字符串设置到消息体中
+        Message<String> message = MessageBuilder.withPayload(JsonUtils.toJsonString(followUserMqDTO)).build();
+
+        // 为MQ消息打上标签Tag： 格式--> Topic : Tag
+        String destination =  MQConstants.TOPIC_FOLLOW_OR_UNFOLLOW + ":" + MQConstants.TAG_FOLLOW;
+        log.info("==> 开始发送关注操作 MQ,消息体: {}",followUserMqDTO);
+
+        // 【生产者】:异步发送MQ消息，表示消息要发送到的主题（Topic）或者队列（Queue）/ 消息 /回调接口，用于处理发送结果
+        rocketMQTemplate.asyncSend(destination, message, new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                log.info("==> MQ 发送成功，SendResult：{}",sendResult);
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                log.error("==> MQ 发送异常 :",throwable );
+            }
+        });
+
         return Response.success();
     }
 
